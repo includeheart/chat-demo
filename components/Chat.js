3030377452bef520c6bc79b8839ca2fc6616fcab
Patrick Kernan <pkernan18@yahoo.com>
2025-05-21 08:47:32 -0400

    Adding updates for tutor

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,44 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
     const { name, bgColor } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend= (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+    }
 
     useEffect(() => {
-        navigation.setOptions({ title: name });
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+        
+        ]);
+    }, []);
+
+    useEffect(() => {
+        navigation.setOptions({ title: name })
     }, []);
 
     return (
         <View style={[styles.container, { backgroundColor: bgColor || '#fff' }]}>
-            <Text style={styles.chatText}>Chat</Text>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+            { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
         </View>
     );
 }
@@ -20,12 +48,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    chatText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#ffffff'
-    },
+    }
 });
 
 export default Chat;
